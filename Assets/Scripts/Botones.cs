5edf99bd521f9e2e62406afaed04e44200cfd1c3
Villegazs <alejitov214@hotmail.com>
2024-05-13 19:51:19 -0500

    Niveles implementados

---
+++ Assets/Scripts/Botones.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Botones : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start()
+    {
+
+        animator = GetComponent<Animator>();
+
+    }
+
+    public void OnClickJugar()
+    {
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+
+    public void OnClickSalir() 
+    {
+        Debug.Log("Salir");
+        Application.Quit();
+    }
+
+    public void OnClickMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
